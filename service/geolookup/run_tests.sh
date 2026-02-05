43e5c7a18683d3623828904c6f2d3424c6f39933
adam87328 <adam1dohnal@gmail.com>
2026-02-05 13:22:25 +0100

    restructure

---
+++ service/geolookup/run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# venv
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Start Uvicorn server in the background and save its PID
+cd ./app
+echo "Starting Uvicorn server..."
+uvicorn main:app --port 8080&
+UVICORN_PID=$!
+cd ..
+
+# Wait for Uvicorn to fully start up (optional, but helpful to avoid timing issues)
+sleep 4
+
+# Run the test script (adjust the path as needed)
+echo "Running test script..."
+python3 -m unittest tests/tests.py
+
+# After the test script finishes, shut down Uvicorn
+echo "Shutting down Uvicorn server..."
+kill $UVICORN_PID
+
+# Confirm the server is stopped
+wait $UVICORN_PID
+
+echo "Test completed and server shut down."
---
+++ service/geolookup/tests/tests.py
@@ -0,0 +1,61 @@
+import unittest
+import requests
+from pathlib import Path
+import json
+
+class TestMicroservice(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        # microservice URL
+        self.url = "http://localhost:8080/"
+    
+    def test_service_up(self):
+        """Test microservice is running, and responds GET"""
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.text,'{"message":"geolookup"}')
+
+    def test_route_takeoffdb(self):
+        """Test takeoff DB with known coordinate"""
+        lat=47.399682
+        lon=9.942572
+        url = self.url + f"/takeoffdb?lat={lat}&lon={lon}"
+
+        response = requests.get(url)
+        self.assertEqual(response.status_code,200)
+
+        d = json.loads(response.text)
+        self.assertEqual( d["name"], "Niedere - Andelsbuch")
+
+    def test_route_nearest_town(self):
+        """Test nearest_town with known coordinate"""
+        lat=47.399682
+        lon=9.942572
+        url = self.url + f"/nearest_town?lat={lat}&lon={lon}"
+
+        response = requests.get(url)
+        self.assertEqual(response.status_code,200)
+
+        d = json.loads(response.text)
+        self.assertEqual( d["city"], "Bizau")
+        self.assertEqual( d["iso_3166_2"], "AT")
+
+    def test_route_admin1(self):
+        """Test nearest_town with known coordinate"""
+        lat=47.399682
+        lon=9.942572
+        url = self.url + f"/admin1?lat={lat}&lon={lon}"
+
+        response = requests.get(url)
+        self.assertEqual(response.status_code,200)
+
+        d = json.loads(response.text)
+        self.assertEqual( d["admin0"], "Austria")
+        self.assertEqual( d["admin1"], "Vorarlberg")
+        self.assertEqual( d["iso_3166_1"], "AT-8")
+        self.assertEqual( d["iso_3166_2"], "AT")
+
+
+if __name__ == '__main__':
+    unittest.main()
